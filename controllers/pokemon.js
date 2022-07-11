bf6e0aaf2a47c4b3da680162be4af0990deea7e3
iAGuss <gustavodario2001@gmail.com>
2022-07-11 15:10:50 -0300

    a

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,7 +1,7 @@
 const listaPokemon = require("../models/listaPokemon");
 
 exports.getPokemon = (req, res) => {
-  const { name, number } = req.query;
+  const { name, number, types } = req.query;
   let pokemon = listaPokemon;
   if (name) {
     pokemon = pokemon.find(
@@ -11,9 +11,23 @@ exports.getPokemon = (req, res) => {
   if (number) {
     pokemon = pokemon.find((pk) => pk.number === number);
   }
-  // if (types) {
-  //   pokemon = pokemon.find((pk) => pk.types === types);
-  // }
+
+
+  if (types) {
+    const [type1, type2] = types.split(",");
+    if (type1) {
+      pokemon = pokemon.filter(
+        (pk) => pk.types[0] === type1 || pk.types[1] === type1
+      );
+    }
+    if (type2) {
+      pokemon = pokemon.filter(
+        (pk) => pk.types[1] === type2 || pk.types[0] === type2
+      );
+    }
+  }
+
+  
 
   res.send(pokemon);
 };
